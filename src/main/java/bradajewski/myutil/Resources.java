d07d84c7ffc4e80ff214e93c6709b8322c03ad6e
Bogumił Radajewski <bogumil.radajewski@gmail.com>
2017-07-04 12:52:02 +0200

    Update Resources.java

--- src/main/java/bradajewski/myutil/Resources.java
+++ src/main/java/bradajewski/myutil/Resources.java
@@ -1,5 +1,22 @@
 package com.bradajewski.myutil;
 
-public class Resources {
+import java.awt.Image;
+import java.awt.Toolkit;
 
+/**
+ * Klasa zawierająca statyczne metody ułatwiające pracę z zasobami w Maven.
+ * @author PRO-WIZJA
+ */
+public class Resources {
+    
+    /**
+     * Metoda umożliwiająca dostęp do obrazów dołączanych przez Maven z folderu 
+     * src/main/resources do archiwum .jar.
+     * 
+     * @param name ścieżka do pliku, np. "images/file.jpg"
+     * @return {@code Image} - pobrany plik 
+     */
+    public final static Image getImage(String name) {
+        return Toolkit.getDefaultToolkit().getImage(Thread.currentThread().getContextClassLoader().getResource(name));
+    } 
 }
