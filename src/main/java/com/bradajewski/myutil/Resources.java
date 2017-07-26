d97ec5287134be997e1ab9815c27db09b6080d4d
bradajewski <PRO-WIZJA@PRO-WIZJA-Vaio>
2017-07-26 12:45:54 +0200

    added getResource()

--- src/main/java/com/bradajewski/myutil/Resources.java
+++ src/main/java/com/bradajewski/myutil/Resources.java
@@ -2,12 +2,22 @@ package com.bradajewski.myutil;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.io.InputStream;
 
 /**
  * Klasa zawierająca statyczne metody ułatwiające pracę z zasobami w Maven.
  * @author Bogumił Radajewski
  */
 public class Resources {
+    
+    /**
+     * Metoda umożliwiająca dostęp do dowolnych zasobów dołączanych do src/main/resources
+     * @param name - nazwa zasobu, np. "file.txt"
+     * @return (@code InputStream} odpowiadający wybranemu zasobowi
+     */
+    public static InputStream getResource(String name) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+    }
     
     /**
      * Metoda umożliwiająca dostęp do obrazów dołączanych z folderu src/main/resources
